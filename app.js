ba290ef78e5e5b104933503fba3f285ac7d47d9a
Dayeon-Hong <dayon0325@gmail.com>
2022-07-12 13:47:02 +0900

    feat:add files

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 10;  
+const b = 1;
+let myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello "+ myName);
+
+myName = "dayon";
+console.log("my new name is "+myName);
